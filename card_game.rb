56710e8513539e8cbf2793de695a5442d3ec6267
Brendan <laotsu@hotmail.co.uk>
2020-03-03 14:52:00 +0000

    Task 1, pseudo coding errors complete

---
+++ card_game.rb
@@ -0,0 +1,38 @@
+### Testing task 2 code:
+
+# Carry out dynamic testing on the code below.
+# Correct the errors below that you spotted in task 1.
+
+class CardGame
+
+  # naming convention incorrect - should be checkForAce
+  def checkforAce(card)
+    if card.value = 1
+      return true
+  # else statement is redundant
+    else
+      return false
+    end
+  end
+
+  # 'dif' spelling error - should be def
+  # card1 and card2 should be separated with a comma
+  dif highest_card(card1 card2)
+  if card1.value > card2.value
+  # should return card1
+    return card
+  else
+    return card2
+  end
+end
+# unnecessary break
+end
+
+def self.cards_total(cards)
+  # total should be assigned a value with the = operator followed by an integer
+  total
+  for card in cards
+    total += card.value
+    return "You have a total of" + total
+  end
+end
